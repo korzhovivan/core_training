2f5f2da9879d321b1e3f47b2c1c85dd50ecf51d2
ko <korzhoff.i@gmail.com>
2019-11-28 15:29:34 +0300

    commit

---
+++ TripTrackerLive/TripTracker.BackService/Models/Repository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TripTracker.BackService.Models
+{
+    public class Repository
+    {
+        private List<Trip> MyTrips = new List<Trip>
+        {
+            new Trip
+            {
+                Id = 0,
+                Name = "MVP Summit",
+                StartDate = new DateTime(2019,3,5),
+                EndTime = new DateTime(2019,3,8)
+            },
+            new Trip
+            {
+                Id = 1,
+                Name = "DevInterSection Orlando 2018",
+                StartDate = new DateTime(2019,3,25),
+                EndTime = new DateTime(2019,3,27)
+            },
+            new Trip
+            {
+                Id = 2,
+                Name = "Build 2019",
+                StartDate = new DateTime(2019,5,7),
+                EndTime = new DateTime(2019,5,9)
+            }
+        };
+
+        public Trip Get(int id)
+        {
+            return MyTrips.First(t => t.Id == id);
+        }
+        public List<Trip> Get()
+        {
+            return MyTrips;
+        }
+
+        public void Add(Trip newTrip)
+        {
+            MyTrips.Add(newTrip);
+        }
+        public void Update(Trip tripToUpdate)
+        {
+            MyTrips.Remove(MyTrips.First(t => t.Id == tripToUpdate.Id));
+            Add(tripToUpdate);
+        }
+        public void Remove(int id)
+        {
+            MyTrips.Remove(MyTrips.First(t => t.Id == id));
+        }
+    }
+}
---
+++ TripTrackerLive/TripTracker.BackService/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace TripTracker.BackService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<Models.Repository>();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(option =>
+                option.SwaggerDoc("v1", new Info { Title = "Trip Tracker", Version = "v1" })
+            );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Trip Tracker v1")
+            );
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+
+        }
+    }
+}
